28289762e5231c2190e38e72322c9ca3e278d3ae
dothanhlam97 <dothanhlam.98@gmail.com>
2017-09-01 00:16:23 +0700

    finish expert challenge

--- 5-Advanced-JS/5-Advanced-JS/script.js
+++ 5-Advanced-JS/5-Advanced-JS/script.js
@@ -21,6 +21,20 @@
  7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
  */
 
+/*
+ --- Expert level ---
+
+ 8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+ 9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+ 10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+ 11. Display the score in the console. Use yet another method for this.
+ */
+
+var currentScore = 0;
+
 var question = {
     name: "",
     options: [],
@@ -64,6 +78,16 @@ question3 = Object.create(question, {
     answer: { value: 'C' }
 });
 
+showScore = function () {
+  console.log("Your current Score is: " + currentScore);
+  console.log("---------------------------------------");
+};
+
+endGame = function () {
+    console.log("Your total Score is: " + currentScore);
+    console.log("Good bye! See you again!");
+}
+
 const listQuestion = [question1, question2, question3];
 
 while (true) {
@@ -74,8 +98,9 @@ while (true) {
     currentQuestion.showQuestion();
     const answer = prompt('Your answer ?');
     if (answer === 'exit') {
-        console.log("Good bye! See you again!");
+        endGame();
         break;
     }
-    currentQuestion.checkAnswer(answer);
+    currentScore += currentQuestion.checkAnswer(answer);
+    showScore();
 }
